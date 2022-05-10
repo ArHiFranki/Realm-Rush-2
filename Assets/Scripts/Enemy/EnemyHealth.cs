b75c9fcb9f84ce39e1eab3b443c8896f1db3fcb9
ArHiFranki <franki550@gmail.com>
2022-05-10 17:55:40 +0700

    Добавил проект в репозиторий

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Enemy))]
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private int maxHitPoints = 5;
+
+    [Tooltip("Adds amount to maxHitPoints when enemy dies")]
+    [SerializeField] private int difficultyRamp = 1;
+
+    private int currentHitPoints = 0;
+    private Enemy enemy;
+
+    private void OnEnable()
+    {
+        currentHitPoints = maxHitPoints;
+    }
+
+    private void Start()
+    {
+        enemy = GetComponent<Enemy>();
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        currentHitPoints--;
+
+        if (currentHitPoints <= 0)
+        {
+            gameObject.SetActive(false);
+            maxHitPoints += difficultyRamp;
+            enemy.RewardGold();
+        }
+    }
+}
---
+++ Assets/Scripts/MoveToEditorFolderBeforeBuild/CoordinateLabeler.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteAlways]
+[RequireComponent(typeof(TextMeshPro))]
+public class CoordinateLabeler : MonoBehaviour
+{
+    [SerializeField] private Color defaultColor = Color.white;
+    [SerializeField] private Color blockedColor = Color.yellow;
+
+    private TextMeshPro label;
+    private Vector2Int coordinates = new Vector2Int();
+    private Waypoint waypoint;
+
+    private void Awake()
+    {
+        label = GetComponent<TextMeshPro>();
+        label.enabled = false;
+
+        waypoint = GetComponentInParent<Waypoint>();
+        DisplayCoordinates();
+    }
+
+    private void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            DisplayCoordinates();
+            UpdateObjectName();
+            label.enabled = true;
+        }
+
+        SetLabelColor();
+        ToggleLabels();
+    }
+
+    private void DisplayCoordinates()
+    {
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+
+        label.text = coordinates.x + "," + coordinates.y;
+    }
+
+    private void UpdateObjectName()
+    {
+        transform.parent.name = coordinates.ToString();
+    }
+
+    private void SetLabelColor()
+    {
+        if (waypoint.IsPlaceable)
+        {
+            label.color = defaultColor;
+        }
+        else
+        {
+            label.color = blockedColor;
+        }
+    }
+
+    private void ToggleLabels()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            label.enabled = !label.IsActive();
+        }
+    }
+}
