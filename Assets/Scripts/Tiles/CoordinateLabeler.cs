13e6dfb1cb497faff09478963903c057ead6435f
ArHiFranki <franki550@gmail.com>
2022-05-16 20:38:44 +0700

    Отключил показ координатной сетки по умолчанию

--- Assets/Scripts/Tiles/CoordinateLabeler.cs
+++ Assets/Scripts/Tiles/CoordinateLabeler.cs
@@ -31,7 +31,6 @@ public class CoordinateLabeler : MonoBehaviour
         {
             DisplayCoordinates();
             UpdateObjectName();
-            label.enabled = true;
         }
 
         SetLabelColor();
